d85414effbab6a5d5a8a0eb620d4f6752bd7bbef
Richard Robinson <rrobinson.campos@gmail.com>
2022-07-05 08:49:25 -0300

    refactor: Creating a contract for db config and implements mysql connection

---
+++ bin/infra/database/mysql_db_configuration.dart
@@ -0,0 +1,29 @@
+import 'package:mysql1/mysql1.dart';
+
+import '../../utils/custom_env.dart';
+import 'db_configuration.dart';
+
+class MySqlDBConfiguration implements DBConfiguration {
+  MySqlConnection? _connection;
+
+  @override
+  Future<MySqlConnection> get connection async {
+    _connection ??= await createConnection();
+
+    if (_connection == null) {
+      throw Exception('[ERROR/DB] -> Failed Create Connection');
+    }
+
+    return _connection!;
+  }
+
+  @override
+  Future<MySqlConnection> createConnection() async =>
+      await MySqlConnection.connect(ConnectionSettings(
+        host: await CustomEnv.get<String>(key: 'DB_HOST'),
+        port: await CustomEnv.get<int>(key: 'DB_PORT'),
+        user: await CustomEnv.get<String>(key: 'DB_USER'),
+        password: await CustomEnv.get<String>(key: 'DB_PASS'),
+        db: await CustomEnv.get<String>(key: 'DB_SCHEMA'),
+      ));
+}
