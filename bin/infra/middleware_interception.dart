50422e823f9c14c8dcc28d45207a4fd4676562d7
Richard Robinson <rrobinson.campos@gmail.com>
2022-07-06 13:12:10 -0300

    feat: Adding CORS middleware for all origins

--- bin/infra/middleware_interception.dart
+++ bin/infra/middleware_interception.dart
@@ -1,9 +1,30 @@
 import 'package:shelf/shelf.dart';
 
 class MiddlewareInterception {
-  Middleware get middleware => createMiddleware(
+  static Middleware get contentTypeJson => createMiddleware(
         responseHandler: (Response res) => res.change(
           headers: {'content-type': 'application/json'},
         ),
       );
+
+  static Middleware get cors {
+    var allowedHeaders = {'Access-Control-Allow-Origin': '*'};
+
+    Response? handlerOption(Request request) {
+      if (request.method == 'OPTIONS') {
+        return Response(200, headers: allowedHeaders);
+      } else {
+        return null;
+      }
+    }
+
+    Response addCorsHeader(Response res) => res.change(
+          headers: allowedHeaders,
+        );
+
+    return createMiddleware(
+      requestHandler: handlerOption,
+      responseHandler: addCorsHeader,
+    );
+  }
 }
